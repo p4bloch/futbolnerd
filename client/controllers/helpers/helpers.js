0fb03f532ef7a798dac5afcd543e5327031d6d59
p4bloch <pchiappetti@gmail.com>
2014-12-18 00:49:00 -0300

    Chat con hora papaaaa

--- client/controllers/helpers/helpers.js
+++ client/controllers/helpers/helpers.js
@@ -6,3 +6,7 @@ scrollChat = function(){
 	var el = $(".chat")[0];
     el.scrollTop = el.scrollHeight;
 }
+
+Template.registerHelper('dateToHuman', function(date){
+	return moment(date).format('D/M h:mm');
+});
